c51eafc2fb2d8930391ebe545a1c5e3b00ca15ec
nardraj <email2wonaresh@gmail.com>
2025-06-20 10:49:24 +1200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,70 @@
+provider "azurerm" {
+  features = {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "lab_rg" {
+  name     = "rg-devops-lab"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "lab-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.lab_rg.location
+  resource_group_name = azurerm_resource_group.lab_rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "lab-subnet"
+  resource_group_name  = azurerm_resource_group.lab_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "lab-nic"
+  location            = azurerm_resource_group.lab_rg.location
+  resource_group_name = azurerm_resource_group.lab_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "lab-public-ip"
+  location            = azurerm_resource_group.lab_rg.location
+  resource_group_name = azurerm_resource_group.lab_rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "labvm"
+  resource_group_name   = azurerm_resource_group.lab_rg.name
+  location              = azurerm_resource_group.lab_rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  admin_password        = "P@ssw0rd1234!"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  custom_data = filebase64("init-script.sh")
+}
